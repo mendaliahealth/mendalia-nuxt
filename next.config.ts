7ff0ffaa0a0fe50bbb4edc4a621a9a5490650b7d
pushpan999 <getpushpan@gmail.com>
2026-06-03 09:20:10 +0545

    next config for static site

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for static export if you use next/image
+  },
+};
 
 export default nextConfig;
