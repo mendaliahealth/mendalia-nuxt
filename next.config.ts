63da5824061f98e5efee3122e53a7ddfa8126176
pushpan999 <getpushpan@gmail.com>
2026-06-03 17:43:04 +0545

    url fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true, // Required for static export if you use next/image
   },
